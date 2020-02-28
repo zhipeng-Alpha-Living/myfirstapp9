04cf849d455e7c876654b28c34a2f9e234f859eb
zhipeng-Alpha-Living <zhipeng@alpha-living.com>
2020-02-28 17:30:31 +0800

    3 files modified, 1 deleted

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,7 +6,7 @@ import { CartService } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
   items;
